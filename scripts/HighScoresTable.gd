aeb09b3eda24534d301f37465066baa212645f01
Ândreo Barros <andreo.dias.barros@gmail.com>
2024-04-03 15:48:00 -0300

    Score table completed, and file cleaning

--- scripts/HighScoresTable.gd
+++ scripts/HighScoresTable.gd
@@ -1,8 +1,9 @@
 extends Control
 class_name Table
 
-@onready var m_NodeTableRows : VBoxContainer = get_node("ScoreScreen/TableMargins/TableContents/TableRows")
+@onready var m_NodeTableRows : Control = get_node("ScoreScreen/TableMargins/TableContents/TableRows")
 @onready var return_to_home = %ReturnToHome
+@onready var table_contents = $ScoreScreen/TableMargins/TableContents
 
 
 func _ready():
@@ -25,6 +26,30 @@ func fill_score_table(dificulty: String):
 
 
 func new_score_row(player_name : String, score : String, time: String, datetime: String) -> void:
-	var new_table_row : TableRow = preload("res://scenes/high_score_table/table_row.tscn").instantiate() as TableRow
-	m_NodeTableRows.add_child(new_table_row)
-	new_table_row.set_row(player_name, score, time, datetime)
+	#var new_table_row : TableRow = preload("res://scenes/high_score_table/table_row.tscn").instantiate() as TableRow
+	#table_contents.add_child(new_table_row)
+	#new_table_row.set_row(player_name, score, time, datetime)
+	var separator1 = VSeparator.new()
+	var value_name = Label.new()
+	value_name.text = player_name
+	value_name.clip_text = true
+	var separator2 = VSeparator.new()
+	var value_score = Label.new()
+	value_score.text = score
+	var separator3 = VSeparator.new()
+	var value_time = Label.new()
+	value_time.text = time
+	var separator4 = VSeparator.new()
+	var value_date = Label.new()
+	value_date.text = datetime
+	var separator5 = VSeparator.new()
+
+	table_contents.add_child(separator1)
+	table_contents.add_child(value_name)
+	table_contents.add_child(separator2)
+	table_contents.add_child(value_score)
+	table_contents.add_child(separator3)
+	table_contents.add_child(value_time)
+	table_contents.add_child(separator4)
+	table_contents.add_child(value_date)
+	table_contents.add_child(separator5)
