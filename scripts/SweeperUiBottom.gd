f99b831b61f6d9e7d39aaeb234e4aaa30f830b3f
andreoBarros <andreo.dias.barros@gmail.com>
2026-02-21 22:06:41 -0300

    Better looking UI

--- scripts/SweeperUiBottom.gd
+++ scripts/SweeperUiBottom.gd
@@ -4,7 +4,13 @@ class_name SweeperUiBottom
 
 @onready var menu_button = %MenuButton
 @onready var hold_timer_menu = %HoldTimerMenu
+@onready var current_theme = %BaseNode.theme
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var styleBox: StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+	styleBox.set("bg_color", current_theme.get_meta("bottom_ui_background_color"))
+	add_theme_stylebox_override("panel", styleBox)
 
 func update_dificulty(dificulty: String, opened_menu: PopupMenu):
 	menu_button.text = "DIFICULTY: " + dificulty.to_upper().split("_")[0]
