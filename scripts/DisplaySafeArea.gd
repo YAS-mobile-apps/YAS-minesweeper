80aaedf9d3b6792ae8f7e125640513373f4ed28d
andreoBarros <andreo.dias.barros@gmail.com>
2026-03-10 22:29:49 -0300

    Tile changes and UI adjustments

--- scripts/DisplaySafeArea.gd
+++ scripts/DisplaySafeArea.gd
@@ -1,8 +1,11 @@
 extends PanelContainer
 
-
+@onready var baseNode = %BaseNode
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GlobalFuncs.avoid_notch(self)
 	GlobalFuncs.avoid_notch_bottom(self)
 
+	var styleBox: StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+	styleBox.set("bg_color", baseNode.theme.get_meta("top_ui_background_color"))
+	add_theme_stylebox_override("panel", styleBox)
